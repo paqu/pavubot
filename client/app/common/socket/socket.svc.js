58451cdeb2437c016ecad3acc7aa72f9ebc728bc
Kusz Pawel <pawel.kusz@nokia.com>
2016-08-06 16:58:23 +0200

    Czyszczenie repo

--- client/app/common/socket/socket.svc.js
+++
@@ -1,5 +0,0 @@
-angular.module('inzApp')
-.factory('Socket',['socketFactory', function (socketFactory) {
-    var myIoSocket = io.connect('/user');
-    return socketFactory({ioSocket:myIoSocket});
-}]);
