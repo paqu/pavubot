7e6c0919c1dfde24acc761d7de0288d1558cdcd0
Kusz Pawel <pawel.kusz@nokia.com>
2016-09-23 15:36:12 +0200

    dodanie obslugi usuwania i dodwania uzytkownika poszukiwanego -- klient

--- client/app/wanted/wanted.controller.js
+++ client/app/wanted/wanted.controller.js
@@ -1,6 +1,35 @@
 angular.module('inzApp')
-    .controller('WantedCtrl', ['$scope','Wanted', function ($scope, Wanted) {
+    .controller('WantedCtrl', ['$scope','$http', 'Wanted', function ($scope, $http, Wanted) {
         $scope.wanted = Wanted.query(); 
+
+        $scope.addToWantedList = addToWantedList;
+        $scope.removeFromWantedList = removeFromWantedList;
+
+
+        function addToWantedList(WantedId) {
+            var id = _findId(WantedId, $scope.wanted);
+            _setWantedStatus(id,true,$scope.wanted);
+            $http.post('/api/wanted/addToList/' + WantedId);
+        }
+
+        function removeFromWantedList(WantedId) {
+            var id = _findId(WantedId, $scope.wanted);
+            _setWantedStatus(id,false,$scope.wanted);
+            $http.delete('/api/wanted/removeFromList/' + WantedId);
+        }
+
+        function _findId(id, arr) {
+            for (var i = 0; i < arr.length; i++) {
+                if (arr[i]._id == id)
+                    return i;
+            }
+            return -1;
+        }
+
+        function _setWantedStatus(id,val,arr) {
+            arr[id].status = val;
+        }
+
     }])
     .controller('WantedViewCtrl', function ($scope, $state, $stateParams, Wanted) {
         $scope.wanted = Wanted.get({id: $stateParams.id});
